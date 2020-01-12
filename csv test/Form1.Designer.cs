f2795d34f7d2b3999780d1e1f45a24307047881d
Marin Miletić <mrnmiletic@gmail.com>
2020-01-12 02:36:54 +0100

    Add project files.

---
+++ csv test/Form1.Designer.cs
@@ -0,0 +1,156 @@
+﻿namespace csv_test
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnOpen = new System.Windows.Forms.Button();
+            this.gridView = new System.Windows.Forms.DataGridView();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.uploadFileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.databaseToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.editToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.gridView)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnOpen
+            // 
+            this.btnOpen.Location = new System.Drawing.Point(12, 36);
+            this.btnOpen.Name = "btnOpen";
+            this.btnOpen.Size = new System.Drawing.Size(75, 23);
+            this.btnOpen.TabIndex = 0;
+            this.btnOpen.Text = "Open File";
+            this.btnOpen.UseVisualStyleBackColor = true;
+            this.btnOpen.Click += new System.EventHandler(this.btnOpen_Click);
+            // 
+            // gridView
+            // 
+            this.gridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.gridView.Location = new System.Drawing.Point(113, 36);
+            this.gridView.Name = "gridView";
+            this.gridView.Size = new System.Drawing.Size(1756, 830);
+            this.gridView.TabIndex = 1;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.databaseToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1904, 24);
+            this.menuStrip1.TabIndex = 2;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.uploadFileToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // uploadFileToolStripMenuItem
+            // 
+            this.uploadFileToolStripMenuItem.Name = "uploadFileToolStripMenuItem";
+            this.uploadFileToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.uploadFileToolStripMenuItem.Text = "Upload File";
+            // 
+            // databaseToolStripMenuItem
+            // 
+            this.databaseToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.editToolStripMenuItem});
+            this.databaseToolStripMenuItem.Name = "databaseToolStripMenuItem";
+            this.databaseToolStripMenuItem.Size = new System.Drawing.Size(67, 20);
+            this.databaseToolStripMenuItem.Text = "Database";
+            // 
+            // editToolStripMenuItem
+            // 
+            this.editToolStripMenuItem.Name = "editToolStripMenuItem";
+            this.editToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.editToolStripMenuItem.Text = "Edit";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 65);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Upload";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(28, 165);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1904, 1041);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.gridView);
+            this.Controls.Add(this.btnOpen);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Read  .csv";
+            ((System.ComponentModel.ISupportInitialize)(this.gridView)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnOpen;
+        private System.Windows.Forms.DataGridView gridView;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem uploadFileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem databaseToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem editToolStripMenuItem;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ csv test/Form1.cs
@@ -0,0 +1,203 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.OleDb;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace csv_test
+{
+    public partial class Form1 : Form
+    {
+
+        private DataTable dt;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public DataTable readCSV(string filePath)
+        {
+            var dt = new DataTable();
+            // Creating the columns
+            File.ReadLines(filePath).Take(1)
+                .SelectMany(x => x.Split(new[] { ',' }, StringSplitOptions.RemoveEmptyEntries))
+                .ToList()
+                .ForEach(x => dt.Columns.Add(x.Trim()));
+
+            // Adding the rows
+            File.ReadLines(filePath).Skip(1)
+                .Select(x => x.Split(','))
+                .ToList()
+                .ForEach(line => dt.Rows.Add(line));
+            return dt;
+        }
+
+        public List<AstronomyEntry> fillSQLEntry(DataTable dt)
+        {
+            List<AstronomyEntry> entries = new List<AstronomyEntry>();
+
+            for (int i = 0; i < 10; i++)
+            {
+                DataRow row = dt.Rows[i];
+                AstronomyEntry entry = new AstronomyEntry();
+                bool validation;
+                validation = int.TryParse(row["id"].ToString(), out int id);
+                entry.IDEntry = id;
+
+                //validation = int.TryParse(row["hip"].ToString(), out int hip);
+                //entry.Hip = hip;
+
+
+                //validation = int.TryParse(row["hd"].ToString(), out int hd);
+                //entry.Hd = hd;
+
+                //validation = int.TryParse(row["hr"].ToString(), out int hr);
+                //entry.Hr = hr;
+
+                //validation = int.TryParse(row["gl"].ToString(), out int gl);
+                //entry.Gl = gl;
+
+                //validation = int.TryParse(row["bf"].ToString(), out int bf);
+                //entry.Bf = bf;
+
+                entry.Proper = row["proper"].ToString();
+
+                //validation = int.TryParse(row["ra"].ToString(), out int ra);
+                //entry.Ra = ra;
+
+                //validation = int.TryParse(row["dec"].ToString(), out int dec);
+                //entry.Dec = dec;
+
+                validation = int.TryParse(row["dist"].ToString(), out int dist);
+                entry.Dist = dist;
+
+                //validation = decimal.TryParse(row["pmra"].ToString(), out decimal pmra);
+                //entry.Pmra = pmra;
+
+                //validation = decimal.TryParse(row["pmdec"].ToString(), out decimal pmdec);
+                //entry.Pmdec = pmdec;
+
+                //validation = decimal.TryParse(row["rv"].ToString(), out decimal rv);
+                //entry.Rv = rv;
+
+                validation = decimal.TryParse(row["mag"].ToString(), out decimal mag);
+                entry.Mag = mag;
+
+                //validation = decimal.TryParse(row["absmag"].ToString(), out decimal absmag);
+                //entry.Absmag = absmag;
+
+                //entry.Spect = row["spect"].ToString();
+
+                //validation = decimal.TryParse(row["ci"].ToString(), out decimal ci);
+                //entry.Ci = ci;
+
+                //validation = decimal.TryParse(row["x"].ToString(), out decimal x);
+                //entry.X = x;
+
+                //validation = decimal.TryParse(row["y"].ToString(), out decimal y);
+                //entry.Y = y;
+
+                //validation = decimal.TryParse(row["z"].ToString(), out decimal z);
+                //entry.Z = z;
+
+                //validation = decimal.TryParse(row["vx"].ToString(), out decimal vx);
+                //entry.Vx = vx;
+
+                //validation = decimal.TryParse(row["vy"].ToString(), out decimal vy);
+                //entry.Vy = vy;
+
+                //validation = decimal.TryParse(row["vz"].ToString(), out decimal vz);
+                //entry.Vz = vz;
+
+                //validation = decimal.TryParse(row["rarad"].ToString(), out decimal rarad);
+                //entry.Rrad = rarad;
+
+                //validation = decimal.TryParse(row["decrad"].ToString(), out decimal decrad);
+                //entry.Decrad = decrad;
+
+                //validation = decimal.TryParse(row["pmrarad"].ToString(), out decimal pmrarad);
+                //entry.Pmrarad = pmrarad;
+
+                //validation = decimal.TryParse(row["pmdecrad"].ToString(), out decimal pmdecrad);
+                //entry.Pmdecrad = pmdecrad;
+
+                //entry.Bayer = row["bayer"].ToString();
+
+                //validation = int.TryParse(row["flam"].ToString(), out int flam);
+                //entry.Flam = flam;
+
+                //entry.Con = row["con"].ToString();
+
+                //validation = int.TryParse(row["comp"].ToString(), out int comp);
+                //entry.Comp = comp;
+
+                //validation = int.TryParse(row["comp_primary"].ToString(), out int comp_primary);
+                //entry.Comp_primary = comp_primary;
+
+                //entry.Base = row["base"].ToString();
+
+                //validation = decimal.TryParse(row["lum"].ToString(), out decimal lum);
+                //entry.Lum = lum;
+
+                //entry.Var = row["var"].ToString();
+                //validation = decimal.TryParse(row["var_min"].ToString(), out decimal var_min);
+
+                //entry.Var_min = var_min;
+                //validation = decimal.TryParse(row["var_max"].ToString(), out decimal var_max);
+                //entry.Var_max = var_max;
+
+                //if (!validation)
+                //{
+                //    throw new Exception("The csv file is not in right format!");
+                //}
+                entries.Add(entry);
+
+            }
+            return entries;
+        }
+
+        private void btnOpen_Click(object sender, EventArgs e)
+        {
+            string selectedFile = "";
+
+            OpenFileDialog fileDialog = new OpenFileDialog();
+            fileDialog.Title = "Open csv file";
+            fileDialog.InitialDirectory = @"c:\";
+            fileDialog.Filter = "All files (*.*)|*.*|All files (*.*)|*.*";
+
+            if (fileDialog.ShowDialog() == DialogResult.OK)
+            {
+                selectedFile = fileDialog.FileName;
+            }
+            //label1.Text = selectedFile;
+           //readCSV(@"c:\users\inetjojo\source\repos\csv parse test\csv parse test\data\hygdata_v3.csv");
+            dt = readCSV(selectedFile);
+            gridView.DataSource = dt;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            fillSQLEntry(dt);
+            using (var db = new AstronomyLogsEntities())
+            {
+                foreach (AstronomyEntry entry in fillSQLEntry(dt))
+                {
+                    label1.Text = entry.IDEntry.ToString();
+                    db.AstronomyEntries.Add(entry);
+                }
+                db.SaveChanges();
+            }
+        }
+    }
+}
+
+
+
